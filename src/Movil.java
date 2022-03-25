db5f6a5f83c7cf80a9990e1bde8043f90401d710
Gorka9898 <glarranagaos20dw@ikzubirimanteo.com>
2022-03-25 11:38:35 +0100

    Empezando

---
+++ src/Movil.java
@@ -0,0 +1,29 @@
+public class Movil {
+    int number;
+    int credit;
+
+public Movil(int number, int credit){
+    this.number=number;
+    this.credit=credit;
+}
+
+public static void display(int number, int credit){
+
+    System.out.print("Number: "+ number+ "\n");
+    System.out.print("Credit: " + credit);
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+}
